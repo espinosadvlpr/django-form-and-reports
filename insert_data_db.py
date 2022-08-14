f4dff32f942261f507964927aeca549c6f24924e
David Espinosa <38819699+espinosadvlpr@users.noreply.github.com>
2022-08-14 09:23:21 -0500

    (Feature): executing db script ready

--- insert_data_db.py
+++ insert_data_db.py
@@ -8,7 +8,6 @@ for indice in range(len(dataFrame)):
     date = datetime.strptime(dataFrame['birthdate'][indice], '%d/%m/%y')
     dataFrame['birthdate'][indice] = str(date.year)+'-'+str(date.month)+'-'+str(date.day)
 
-
 tableName = "survey_data_csv"
 sqlEngine = create_engine(
     'mysql+pymysql://root:a1d2m3i4n@127.0.0.1/gestion_datos', pool_recycle=3600)
@@ -22,7 +21,6 @@ except Exception as ex:
     print(ex)
 else:
     print("Data successfully charged to table:", tableName)
+    dbConnection.execute("insert into survey_form_survey (name, age, birthdate, city, extra_act, height, last_name, lives_with, phisical_act, sleep_hours, subject, subject_grade, weight) select name, age, birthdate, city, extra_act, height, last_name, lives_with, phisical_act, sleep_hours, subject, subject_grade, weight from survey_data_csv;")
 finally:
     dbConnection.close()
-"""
-"""
